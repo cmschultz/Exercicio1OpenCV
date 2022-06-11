75b015b54d337dbd4cd35653829e69588988ce06
cmschultz <caioschultz@unifei.edu.br>
2022-06-11 00:53:33 -0300

    .

---
+++ exercicio.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+import numpy as np
+
+base = cv.imread('./imagem/base.png')
+
+median = cv.medianBlur(base, 9)
+
+
+hsv_median = cv.cvtColor(median, cv.COLOR_BGR2HSV)
+lowRedFilter = cv.inRange(hsv_median, (0, 0, 10), (15, 255, 200))
+highRedFilter = cv.inRange(hsv_median, (160, 190, 50), (179, 255, 255))
+redSelect = cv.add(lowRedFilter, highRedFilter)
+
+result = cv.bitwise_and(base, base, mask=redSelect)
+
+cv.imshow('result', result)
+cv.waitKey(0)
+cv.destroyAllWindows()
